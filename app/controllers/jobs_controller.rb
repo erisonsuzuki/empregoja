8436958fb64ee6e6504859c1eced69d8c4f85a0a
Erison Suzuki <erisonsuzuki@gmail.com>
2016-11-04 16:56:20 -0200

    Feature: User register a new job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,26 @@
 class JobsController < ApplicationController
+  before_action :target_job, only: [:show]
+
   def show
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(job_params)
+
+    redirect_to job_path(@job)
+  end
+
+  private
+
+  def target_job
     @job = Job.find params[:id]
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :location, :category, :company, :description)
   end
 end
