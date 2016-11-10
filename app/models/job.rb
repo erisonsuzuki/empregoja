356019e9c9ba6b8b8c81d4201c8ffae275af0581
Erison Suzuki <erisonsuzuki@gmail.com>
2016-11-10 16:18:42 -0200

    Feature: Hide Expired jobs in homepage Adds message in expired job page

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,9 +2,9 @@ class Job < ApplicationRecord
   belongs_to :company
 	belongs_to :category
 
-	validates :title, presence: true
-  validates :category, presence: true
-  validates :description, presence: true
-  validates :location, presence: true
-  validates :company_id, presence: true
+	validates :title, :category, :description, :location, :company_id, presence: true
+
+  def expired?
+    90.days.ago > created_at
+  end
 end
