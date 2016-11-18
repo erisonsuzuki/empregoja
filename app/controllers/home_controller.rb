839a7ccdeee7302f22bbf73c7538d0e01150fa40
Erison Suzuki <erisonsuzuki@gmail.com>
2016-11-18 17:12:41 -0200

    Fix: recent job container

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @jobs = Job.order('created_at desc')
+    @jobs = Job.all.order('created_at desc')
     @companies = Company.all
    	@categories = Category.all
   end
